8057f188fcc99d93867eeba2a49c52003c7502e8
Sidinei Silva <sidinei.silva02@gmail.com>
2021-12-28 08:45:31 -0300

    fix: :bug: Fix update repository
    Corrigindo atualização de repositorio

--- src/index.js
+++ src/index.js
@@ -32,7 +32,11 @@ app.put("/repositories/:id", (request, response) => {
   const { id } = request.params;
   const updatedRepository = request.body;
 
-  repositoryIndex = repositories.findindex(repository => repository.id === id);
+  if (updatedRepository.likes) {
+    delete updatedRepository.likes
+  }
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
   if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
