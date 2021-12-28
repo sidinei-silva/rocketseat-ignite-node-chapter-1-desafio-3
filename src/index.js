258b704a55eba1c6e8a0e8b800f07d0acc0f8ea5
Sidinei Silva <sidinei.silva02@gmail.com>
2021-12-28 08:46:43 -0300

    fix: :bug: Fix like respository
    Corrigindo like em repositorio

--- src/index.js
+++ src/index.js
@@ -66,7 +66,7 @@ app.delete("/repositories/:id", (request, response) => {
 app.post("/repositories/:id/like", (request, response) => {
   const { id } = request.params;
 
-  repositoryIndex = repositories.findIndex(repository => repository.id === id);
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
   if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
@@ -74,7 +74,7 @@ app.post("/repositories/:id/like", (request, response) => {
 
   const likes = ++repositories[repositoryIndex].likes;
 
-  return response.json('likes');
+  return response.json({ likes });
 });
 
 module.exports = app;
